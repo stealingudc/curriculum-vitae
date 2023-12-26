8df982dec15bcb141f97b0411e758c3c66438432
stealingudc <vladimirdamian23@gmail.com>
2023-12-26 22:09:46 +0200

    Vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/curriculum-vitae/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
